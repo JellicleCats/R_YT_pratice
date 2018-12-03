b82650e18cd7acf6e04120dc5c4c8d3aa9e5ffe2
Jiyeon Jung <32950838+JellicleCats@users.noreply.github.com>
2018-12-03 14:48:15 -0800

    120318

---
+++ R_12_chracter_string1.R
@@ -0,0 +1,33 @@
+y <- character()
+y
+length(y)
+y2 <-""
+y2
+class(y)
+class(y2)
+length(y)
+length(y2)
+
+y <- c("e","12","2")
+y  <- character(10)
+y[3] <- "third"
+y[12] <- "twelveth"
+y
+y[11] <- "11"
+y
+
+n=3
+m="3"
+
+is.character(n)
+is.character(m)
+
+class(n)
+class(m)
+
+n2 <- as.character(n)
+class(n)
+
+t <- (1:5)
+
+
